1ee507433dc513dc863e994c2b36edb2178a3687
apicici <apicici@apicici.com>
2021-07-23 09:37:18 -0400

    added appveyor files

---
+++ .appveyor/build.sh
@@ -0,0 +1,14 @@
+pip install hererocks
+
+export PATH="$PATH:$HOME/.local/bin"
+
+hererocks env --lua 5.1 -rlatest
+source env/bin/activate
+luarocks install moonscript
+
+mkdir -p pathfun
+cd src && moonc -t ../pathfun .
+cd ..
+zip -r build.zip pathfun
+zip -j build.zip *.md
+zip -j build.zip .appveyor/*.md
